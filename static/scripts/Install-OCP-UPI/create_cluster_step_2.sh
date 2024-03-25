6b062637d1116f96f9fd63a5a7767dbe3ff811af
Joe <jcosenz@us.ibm.com>
2024-03-25 17:23:35 -0500

    fixed scripts

--- static/scripts/Install-OCP-UPI/create_cluster_step_2.sh
+++ static/scripts/Install-OCP-UPI/create_cluster_step_2.sh
@@ -21,7 +21,7 @@ wait_for_stack_completion() {
     do
         status=$(aws cloudformation describe-stacks --stack-name "${stack_name}" | jq '.Stacks[].StackStatus' | sed -e 's/"//g')
         if [[ "${status}" == "CREATE_COMPLETE" ]]
-        thenx
+        then
             done=1
             rc=0
         elif [[ "${status}" == "CREATE_IN_PROGRESS" ]]
