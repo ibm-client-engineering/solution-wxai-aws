3a0d4f9528ac8491432bb310f281772f836a5574
oscar-ricaud <oscar.ricaud@ibm.com>
2024-06-04 12:50:19 -0500

    removed /ALPHA

--- check_sensitive_keywords.sh
+++ check_sensitive_keywords.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Check if the environment variable GIT_ENV_VAR_S3_IBM_TXT_URL is set
-if [ -z "${GIT_ENV_ VAR_S3_IBM_TXT_URL+ x}" ]; then
+if [ -z "${GIT_ENV_VAR_S3_IBM_TXT_URL+ x}" ]; then
   echo "Error: GIT_ ENV_VAR_S3_IBM_TXT_URL environment variable is not set. You must update/define the env var with the S3 URL"
   exit 1
 fi
@@ -21,14 +21,13 @@ fi
 echo "Length of SENSITIVE_KEYWORDS_ARRAY: ${#SENSITIVE_KEYWORDS_ARRAY[@]}"
 
 while read -r commit_message; do
-    echo "Committing: $commit_message"
-    for file in $(git diff --name-only); do
-        git checkout $file   # Checkout the latest version of the file
-        echo "File contents: $(git show $file)"
-        if grep -riE "(?i)${SENSITIVE_KEYWORDS_ARRAY[@]}" "$file"; then
-            echo "Error: Sensitive keyword found in file '$file'. Please remove it before committing."
-            exit 1
-        fi
-    done
-     # If no sensitive keywords found, allow commit  (no error)
-done
+  for file in $(git diff --name-only); do
+    git checkout $file # Checkout the latest version of the file
+    if grep -riE "(?i)${SENSITIVE_KEYWORDS_ARRAY[@]}" "$file"; then
+      echo "Error: Sensitive keyword found in file '$file'. Please remove it before committing."
+      exit 1
+    fi
+  done
+done <<< "$(git log -1 --pretty=%s)"
+
+
