114b49369a72c48c171d53bfa7d6237655b8a83f
oscar-ricaud <oscar.ricaud@ibm.com>
2024-06-04 11:54:57 -0500

    added check_sensitive_keywords.sh and pre-commit-config.yaml file

---
+++ check_sensitive_keywords.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+if [ -z "${GIT_ENV_VAR_S3_IBM_TXT_URL+x}" ]; then
+  echo "Error: GIT_ENV_VAR_S3_IBM_TXT_URL environment variable is not set. You must update/define the env var with the S3 URL"
+  exit 1
+fi
+
+# Download sensitive keywords from remote server
+SENSITIVE_KEYWORDS_ARRAY=()
+while read -r line; do
+    SENSITIVE_KEYWORDS_ARRAY+=("$line")
+done < <(curl -s ${GIT_ENV_VAR_S3_IBM_TXT_URL})
+
+# Check if the array is empty
+if [ ${#SENSITIVE_KEYWORDS_ARRAY[@]} -eq 0 ]; then
+    echo "Error: No sensitive keywords found."
+    exit 1
+fi
+
+echo "Length of SENSITIVE_ KEYWORDS_ARRAY: ${#SENSITIVE_ KEYWORDS_ARRAY[@]}"
+
+# Check modified files for sensitive keywords
+for file in $(git ls-files -m); do
+    git checkout $file  # Checkout the latest version of the file
+    echo "File contents: $(git show :$file)"
+    if [ ! -f "$file" ]; then
+        continue
+    fi
+    for keyword in "${SENSITIVE_KEYWORDS_ARRAY[@]}"; do
+        if grep -q "$keyword" "$file"; then
+            echo "Error: Sensitive keyword '$keyword' found in file '$file'. Please remove it before committing."
+            exit 1
+        fi
+    done
+done
+
+# If no sensitive keyword found, allow commit
+exit 0
+
+
