3f302f1f701dabcbb0c7d18109541de24bdc4a81
oscar-ricaud <oscar.ricaud@ibm.com>
2024-06-14 10:28:46 -0500

    this commit should fail

--- src/components/BrowserWindow/index.tsx
+++ src/components/BrowserWindow/index.tsx
@@ -5,7 +5,6 @@
  * LICENSE file in the root directory of this source tree.
  */
 import React, {type CSSProperties, type ReactNode} from 'react';
-THETA
 import clsx from 'clsx';
 import styles from './styles.module.css';
 
